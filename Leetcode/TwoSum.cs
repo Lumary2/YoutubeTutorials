abfe996b4ff09cb986e0e2ccfe8dbc2fc090b62b
Anja Bender <46068592+Lumary2@users.noreply.github.com>
2022-01-22 11:33:00 +0100

    Update TwoSum.cs

--- Leetcode/TwoSum.cs
+++ Leetcode/TwoSum.cs
@@ -3,13 +3,12 @@ public class Solution {
         
         
         int []result=new int[2];
-        int[]numbers=nums;
         
         for(int i=0;i<nums.Length;i++){
             
             for(int j=i+1;j<nums.Length;j++){
                 
-                if(numbers[i]+numbers[j]==target){
+                if(nums[i]+nums[j]==target){
                     result[0]=i;
                     result[1]=j;
                     return result;
